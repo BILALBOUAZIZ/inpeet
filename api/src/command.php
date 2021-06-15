8272b986eaaddb21f241ee46a2d83a0ef8286ffe
Houda saira <houdasaira@gmail.com>
2021-06-15 19:31:08 +0200

    commande

--- api/src/command.php
+++ api/src/command.php
@@ -10,6 +10,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 class command
 {
+
     public $db;
 
     function __construct($container)
@@ -23,7 +24,7 @@ class command
 
         $query = $this->db->prepare("select * from commande ");
         $query->execute();
-        $retour = $query->fetchAll(PDO::FETCH_ASSOC);
+        $retour = $query->fetchAll();
         return $response->withJson($retour);
     }
 
@@ -42,17 +43,30 @@ class command
     {
         $body = $request->getParsedBody();
 
-        $query = $this->db->prepare("INSERT INTO commande VALUES(:prix, .... )");
-        $query->execute(array(':prix' => $body['prix']));
-        $retour = $query->fetch();
-        return $response->withJson($retour);
+        $query = $this->db->prepare("INSERT INTO commande VALUES(:id_client,:id_user,:prix_commande )");
+        $query->execute(array(':id_client' => $body['client ']));
+        $query->execute(array(':id_user' => $body['user']));
+        $query->execute(array(':prix_commande' => $body['prix']));
+       
+
+
+        if ($query) {
+
+            $data = ['message'=> 'Commande ajoutee avec succes','status'=>200];
+        } else {
+            $data = ['message' => 'Erreur ', 'status' =>400 ];
+           
+        }
+
+          return $response->withJson($data);
     }
+
 
     public function update_command(Request $request, Response $response, array $args)
     {
         $body = $request->getParsedBody();
-        $query = $this->db->prepare("UPDATE commande SET prix= :prix ,  WHERE id_commande = :id_commande");
-        $query->execute(array(':id_commande' => $args['id'],"prix" => $body["prix"]));
+        $query = $this->db->prepare("UPDATE commande SET prix= :prix ,   WHERE id_commande = :id_commande");
+        $query->execute(array(':id_commande' => $args['id'], "prix" => $body["prix"]));
         $retour = $query->fetch();
         return $response->withJson($retour);
     }
@@ -60,9 +74,17 @@ class command
     public function delete_command(Request $request, Response $response, array $args)
     {
 
-        $query = $this->db->prepare("select * from commande where id_commande= :id_commande");
+        $query = $this->db->prepare("DELETE FROM commande WHERE id_commande :id_commande");
         $query->execute(array(':id_commande' => $args['id']));
-        $retour = $query->fetch();
-        return $response->withJson($retour);
+        
+        if ($query) {
+
+            $data = ['message'=> 'Commande supprimee avec succes','status'=>200];
+        } else {
+            $data = ['message' => 'La commande est introuvable ', 'status' =>400 ];
+           
+        }
+
+          return $response->withJson($data);
     }
 }
